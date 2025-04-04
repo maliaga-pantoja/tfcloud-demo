6dea18752f93a5249df945d6df18e57eab1f8481
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2025-04-04 14:42:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "eastus2"
 }
+
+
+module "aware_naming" {
+  source = "WeAreRetail/naming/azurerm"
+
+  location    = "France Central"
+  environment = "DEV"
+  project     = "PRJ"
+  area        = "master"
+}
